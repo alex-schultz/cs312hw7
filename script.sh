d3a90c7000c0c5f257883f11152e8bd00ee3326f
alex-schultz <schultzyy911@gmail.com>
2018-06-03 18:23:51 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,17 +8,17 @@ curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/playbook.yam
 curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/server.js > server.js
 chmod 600 ~/id_rsa
 chmod 644 ~/id_rsa.pub
-scp ~/id_rsa.pub root@192.168.1.xxx:/root/.ssh/authorized_keys
+scp ~/id_rsa.pub root@192.168.1.42:/root/.ssh/authorized_keys
 cp ~/id_rs* ~/.ssh
 sudo yum install -y ansible
 ansible-playbook playbook.yaml -i hosts.ini
-curl 192.168.1.100:8080 >> results
-curl 192.168.1.101:8080 >> results
-curl 192.168.1.102:8080 >> results
-curl 192.168.1.103:8080 >> results
-curl 192.168.1.104:8080 >> results
-curl 192.168.1.105:8080 >> results
-curl 192.168.1.106:8080 >> results
-curl 192.168.1.107:8080 >> results
-curl 192.168.1.108:8080 >> results
-curl 192.168.1.109:8080 >> results
+curl 192.168.1.100 >> results
+curl 192.168.1.101 >> results
+curl 192.168.1.102 >> results
+curl 192.168.1.103 >> results
+curl 192.168.1.104 >> results
+curl 192.168.1.105 >> results
+curl 192.168.1.106 >> results
+curl 192.168.1.107 >> results
+curl 192.168.1.108 >> results
+curl 192.168.1.109 >> results
