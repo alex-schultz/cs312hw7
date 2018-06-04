4fa44f17ab879beff9eff3b81907c519e56c9d00
alex-schultz <schultzyy911@gmail.com>
2018-06-03 17:54:07 -0700

    Update hw7.sh

--- hw7.sh
+++ hw7.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-curl http://web.engr.oregonstate.edu/~schulale/cs312/ansible.cfg > ansible.cfg
-curl http://web.engr.oregonstate.edu/~schulale/cs312/ansible-index.html > ansible-index.html
-curl http://web.engr.oregonstate.edu/~schulale/cs312/hosts.ini > hosts.ini
-curl http://web.engr.oregonstate.edu/~schulale/cs312/id_rsa > id_rsa
-curl http://web.engr.oregonstate.edu/~schulale/cs312/id_rsa.pub > id_rsa.pub
-curl http://web.engr.oregonstate.edu/~schulale/cs312/playbook.yaml > playbook.yaml
-curl http://web.engr.oregonstate.edu/~schulale/cs312/server.js > server.js
+curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/ansible.cfg > ansible.cfg
+curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/ansible-index.html > ansible-index.html
+curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/hosts.ini > hosts.ini
+curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/id_rsa > id_rsa
+curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/id_rsa.pub > id_rsa.pub
+curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/playbook.yaml > playbook.yaml
+curl https://raw.githubusercontent.com/alex-schultz/cs312hw7/master/server.js > server.js
 chmod 600 ~/id_rsa
 chmod 644 ~/id_rsa.pub
 scp ~/id_rsa.pub root@192.168.1.xxx:/root/.ssh/authorized_keys
